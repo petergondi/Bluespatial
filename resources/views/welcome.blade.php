11d9e0c90670d013c3f14650574fca8da2ae328e
petergondi <gondipeters@gmail.com>
2018-12-17 20:28:50 +0300

    updated welcome page slider

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 @include('inc.navbar')
 @extends('layouts.app')
 
-  <div class="jumbotron text-center body-main">
+  <div class="text-center body-main">
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -13,21 +13,21 @@
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img class="d-block w-100" src="https://www.quirkybyte.com/wp-content/uploads/2016/02/Green-Nature-Trees-l.jpg" style="height:40%; margin:0px;" alt="First slide">
+            <img class="d-block w-100" src="http://www.host2post.com/images/S7zofXFx1I4HPM~/1920x1200/download/wallpapers-road-nature-green-background-1920x1200.jpg" style="height:40%; margin:0px;" alt="First slide">
             <div class="carousel-caption d-none d-md-block">
                 <h5>Bluespatial Gis solutions</h5>
                 <p>Owesome</p>
               </div>
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="https://www.quirkybyte.com/wp-content/uploads/2016/02/Green-Nature-Trees-l.jpg" style="height:40%;margin:0px;" alt="Second slide">
+            <img class="d-block w-100" src="https://i.ytimg.com/vi/5FSPHNhAHa8/hqdefault.jpg" style="height:40%;margin:0px;" alt="Second slide">
             <div class="carousel-caption d-none d-md-block">
                 <h5>Bluespatial Gis solutions</h5>
                 <p>Owesome</p>
               </div>
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="https://www.quirkybyte.com/wp-content/uploads/2016/02/Green-Nature-Trees-l.jpg" style="height:40%; margin:0px;" alt="Third slide">
+            <img class="d-block w-100" src="https://ak7.picdn.net/shutterstock/videos/10902887/thumb/7.jpg" style="height:40%; margin:0px;" alt="Third slide">
             <div class="carousel-caption d-none d-md-block">
                 <h5>Bluespatial Gis solutions</h5>
                 <p>Owesome</p>
@@ -43,6 +43,5 @@
           <span class="sr-only">Next</span>
         </a>
       </div>
-  </div>
 </html>
   
